d944cc52e3863c5b434344603b7ec0d28aefef9e
ChornChhery <chherychorn@gmail.com>
2025-08-19 17:03:48 +0700

    Streamlit Machine Learning

--- app.py
+++ app.py
@@ -1,3 +1,45 @@
+import streamlit as st
+import numpy as np
+import joblib
 
 
-#['bed', 'bath', 'house_size']
+scaler = joblib.load('scaler.pkl')
+
+model = joblib.load('model.pkl')
+
+
+st.title("Real Estate Price Prediction App")
+
+
+st.divider()
+
+bed = st.number_input("Enter the number of bedrooms", value=2, step = 1)
+bath = st.number_input("Enter the number of bathrooms", value=1, step = 1)
+size = st.number_input("Enter the size of the house in sqft", value=1000, step = 50)
+
+
+X = [bed,bath,size]
+
+st.divider()
+
+
+y_pred = st.button("Predict!")
+
+
+st.divider()
+
+if y_pred:
+
+    st.balloons()
+
+    X1 = np.array(X)
+
+    X_array = scaler.transform([X1])
+
+    prediction = model.predict(X_array)[0]
+
+
+    st.write(f"The predicted price of the house is ${prediction:,.2f}")
+
+else:
+    st.write("Please enter the number of bedrooms, bathrooms, and size of the house to predict the price.")
